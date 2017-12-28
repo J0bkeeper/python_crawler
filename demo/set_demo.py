8c508866d597321b05d038528ce807c104f92227
Jinlong Quan <ovwane@gmail.com>
2017-12-28 17:24:37 +0800

    学习set集合的用法

---
+++ demo/set_demo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'ovwane'
+__time__ = '2017.12.28 12:55'
+
+
+pages = set()
+pages.add("http://www.chinadaily.com.cn/a/201712/10/WS5a2cb288a310eefe3e9a13c7.html")
+pages.add("http://www.chinadaily.com.cn/a/201712/10/WS5a2cb288a310eefe3e9a13c7_2.html")
+pages.add("http://www.chinadaily.com.cn/a/201712/10/WS5a2cb288a310eefe3e9a13c7_1.html")
+
+for p in pages:
+    print(p)
